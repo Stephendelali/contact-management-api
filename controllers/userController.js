8c9d27c124869f04d9570c555536aee4fbfb8619
Stephen Delali <120380863+Stephendelali@users.noreply.github.com>
2025-03-29 16:51:20 +0000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");  // Changed variable name to uppercase
+const User = require("../models/userModel"); 
 
 // @desc Register a new user
 //@route POST /api/users/register
